20d3136dcec458e6f736de81148e1f6fc8f7e44b
RachealNyaga <email@example.com>
2023-02-28 09:33:12 +0300

    add the files

---
+++ src/component/todo.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+
+function Todo() {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleAddTodo = () => {
+    if (inputValue !== '') {
+      const newTodo = { id: Date.now(), text: inputValue, completed: false };
+      setTodos([...todos, newTodo]);
+      setInputValue('');
+    }
+  };
+
+  const handleDeleteTodo = (id) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+  };
+
+  const handleToggleComplete = (id) => {
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, completed: !todo.completed };
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div>
+      <h2>Todo List</h2>
+      <input type="text" value={inputValue} onChange={handleInputChange} />
+      <button onClick={handleAddTodo}>Add Todo</button>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id} style={{ background: todo.completed ? 'green' : 'none' }}>
+            {todo.text}
+            <div className='button'><button onClick={() => handleDeleteTodo(todo.id)}>Delete</button>
+            <button onClick={() => handleToggleComplete(todo.id)}>
+              {todo.completed ? 'uncompleted' : 'completed'}
+            </button>
+            </div>
+            
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Todo;
